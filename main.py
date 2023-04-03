d08b5427160ae666fbf925f218034ec0c87400d0
fitakabisil <112487979+fitakabisil@users.noreply.github.com>
2023-04-03 09:21:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,7 @@ import random
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
-COLORS = []
-
-import arcade
-import random
-
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+COLORS_R = [arcade.color.AZURE, arcade.color.AMBER, arcade.color.AMERICAN_ROSE ]
 
 COLORS_C = [arcade.color.BLUE, arcade.color.FANDANGO_PINK,
 arcade.color.FRENCH_ROSE, arcade.color.GOLDEN_POPPY]
@@ -26,15 +20,17 @@ class Cercle:
         self.change_x = 3
         self.change_y = 3
 
+    def update(self):
+        pass
+
     def draw(self):
         # arcade.draw_circle_filled(center_x, center_y, rayon, color)
         arcade.draw_circle_filled(self.centre_x, self.centre_y, self.rayon, self.color)
 
 
-class Rectangle:
+class Rectangle():
 
-    def __int__(self, x, y, l, h, c):
-
+    def __init__(self, x, y, l, h, c):
         self.center_x = x
         self.center_y = y
         self.longeur = l
@@ -42,6 +38,9 @@ class Rectangle:
         self.color = c
         self.changer_x = 4
         self.changer_y = 4
+
+    def update(self):
+        pass
 
     def draw(self):
         # arcade.draw_rectangle_filled(center_x, center_y, width, height, color)
@@ -58,16 +57,20 @@ class MyGame(arcade.Window):
         # remplir la liste avec 20 objets de type Cercle
         for cercle in range(20):
             rayon = random.randint(10, 30)
-            center_x = random.randint(0 + rayon, SCREEN_WIDTH - rayon)
-            center_y = random.randint(0 + rayon, SCREEN_HEIGHT - rayon)
-            color = random.choice(COLORS_C)
-            cercle = Cercle(rayon, center_x, center_y, color)
+            centre_x = random.randint(0 + rayon, SCREEN_WIDTH - rayon)
+            centre_y = random.randint(0 + rayon, SCREEN_HEIGHT - rayon)
+            couleur = random.choice(COLORS_C)
+            cercle = Cercle(rayon, centre_x, centre_y, couleur)
             self.liste_cercles.append(cercle)
 
         for rectangle in range(20):
-
-            arcade.draw_rectangle_filled(10,10,10,10, (arcade.color.BLUE))
-
+            hauteur = random.randint(10, 30)
+            longeur = random.randint(10, 30)
+            center_x = random.randint(0 + longeur, SCREEN_WIDTH - longeur)
+            center_y = random.randint(0 + hauteur, SCREEN_HEIGHT - hauteur)
+            color = random.choice(COLORS_R)
+            rectangle = Rectangle(hauteur, longeur, center_x, center_y, color)
+            self.liste_rectangles.append(rectangle)
 
     def on_update(self, delta_time: float):
         pass
